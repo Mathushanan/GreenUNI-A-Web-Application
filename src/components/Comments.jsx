0d8ab9a0c3c189a3e8a6ddf69f717858c4e7391e
Mathushanan Mahendrarajah <posttoday0@gmail.com>
2024-02-13 00:19:22 +0530

    Comments component completed

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -3,28 +3,31 @@ import { useState } from "react";
 import { BsThreeDots } from "react-icons/bs";
 import Actions from "./Actions";
 
-const Comment = ({ reply, lastReply }) => {
+const Comment = ({ userAvatar, createdAt, comment, userName, likes }) => {
     const [liked, setLiked] = useState(false);
 
     return (
         <>
             <Flex gap={4} py={2} my={2} w={"full"}>
-                <Avatar src="/zuck-avatar.png" size={"sm"} />
+                <Avatar src={userAvatar} size={"sm"} />
                 <Flex gap={1} w={"full"} flexDirection={"column"}>
                     <Flex w={"full"} justifyContent={"space-between"} alignItems={"center"}>
                         <Text fontSize='sm' fontWeight='bold'>
-                            mark
+                            {userName}
                         </Text>
 
                         <Flex gap={2} alignItems={"center"}>
-                            <Text fontSize={"sm"} color={"gray.light"} >id</Text>
+                            <Text fontSize={"sm"} color={"gray.light"} >
+                                {createdAt}
+
+                            </Text>
                             <BsThreeDots />
                         </Flex>
                     </Flex>
-                    <Text>Looks great!</Text>
+                    <Text>{comment}</Text>
                     <Actions liked={liked} setLiked={setLiked} />
                     <Text fontSize={"sm"} color={"gray.light"}>
-                        {100 + (liked ? 1 : 0)} likes
+                        {likes + (liked ? 1 : 0)} likes
                     </Text>
                 </Flex>
             </Flex>
