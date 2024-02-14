763b65b9f901016e65f7d7e07096bd3fcf560931
Mathushanan Mahendrarajah <posttoday0@gmail.com>
2024-02-14 23:00:04 +0530

    User routes & controllers created

--- Back-End/server.js
+++ Back-End/server.js
@@ -12,6 +12,14 @@ connectDB();
 const app=express();
 const PORT=process.env.PORT || 5000;
 
+app.use(express.json()); 
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/messages", messageRoutes);
+
 app.listen(PORT,()=>{
     console.log(`Server started at http://localhost:${PORT}`);
 })
