e8946d059b763c9b32857437fad1aa788be9d5d1
Mathushanan Mahendrarajah <posttoday0@gmail.com>
2024-02-16 21:30:00 +0530

    Signup function implemented

--- Back-End/server.js
+++ Back-End/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import connectDB from "./db/connectDB.js";
+import userRoutes from "./routes/userRoutes.js";
+
+
 
 
 
@@ -17,8 +20,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
-app.use("/api/posts", postRoutes);
-app.use("/api/messages", messageRoutes);
+
 
 app.listen(PORT,()=>{
     console.log(`Server started at http://localhost:${PORT}`);
