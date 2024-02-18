c549bc35bfa8960266d50fa82ede7eb54f043622
Mathushanan Mahendrarajah <posttoday0@gmail.com>
2024-02-18 12:06:01 +0530

    Get post function implemented

--- Back-End/controllers/postController.js
+++ Back-End/controllers/postController.js
@@ -40,4 +40,42 @@ const createPost = async (req, res) => {
 	}
 };
 
-export {createPost};
+const getPost = async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+
+		if (!post) {
+			return res.status(404).json({ error: "Post not found" });
+		}
+
+		res.status(200).json(post);
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+};
+
+const deletePost = async (req, res) => {
+	try {
+		const post = await Post.findById(req.params.id);
+		if (!post) {
+			return res.status(404).json({ error: "Post not found" });
+		}
+
+		if (post.postedBy.toString() !== req.user._id.toString()) {
+			return res.status(401).json({ error: "Unauthorized to delete post" });
+		}
+
+		if (post.img) {
+			const imgId = post.img.split("/").pop().split(".")[0];
+			await cloudinary.uploader.destroy(imgId);
+		}
+
+		await Post.findByIdAndDelete(req.params.id);
+
+		res.status(200).json({ message: "Post deleted successfully" });
+	} catch (err) {
+		res.status(500).json({ error: err.message });
+	}
+};
+
+export {createPost,getPost,deletePost};
--- Back-End/routes/postRoutes.js
+++ Back-End/routes/postRoutes.js
@@ -1,14 +1,15 @@
 import express from "express";
 import {
-	createPost
+	createPost,getPost,deletePost
 } from "../controllers/postController.js";
 import protectRoute from "../middleWares/protectRoute.js";
 
 
 const router = express.Router();
 
-
+router.get("/:id", getPost);
 router.post("/create",protectRoute, createPost);
+router.delete("/:id", protectRoute, deletePost);
 
 
 export default router;
