f7d122a504aac47907be8623928d1a790e33d5d5
Mathushanan Mahendrarajah <posttoday0@gmail.com>
2024-03-03 22:46:35 +0530

    Chat UI & Back-End Api created

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,21 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
 import connectDB from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
 import { v2 as cloudinary } from "cloudinary";
-
-
-
-
+import { app, server } from "./socket/socket.js";
 
 dotenv.config();
 
 connectDB();
 
-const app=express();
-const PORT=process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -23,14 +20,14 @@ cloudinary.config({
 	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-app.use(express.json({ limit: "50mb" })); 
-app.use(express.urlencoded({extended:true}));
+// Middlewares
+app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
+app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
 app.use(cookieParser());
 
+// Routes
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/messages", messageRoutes);
 
-
-app.listen(PORT,()=>{
-    console.log(`Server started at http://localhost:${PORT}`);
-})
+server.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
